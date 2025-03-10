b78ecb9fa7b33da21721e0d1d6a426033b48c58b
JOSE ANGEL LUGO ESPARZA <lugo.joseangel.c66@gmail.com>
2025-03-10 07:52:21 -0600

    Sidebar responsive

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { AuthContextProvider } from './context/AuthContext';
 import { createContext, useState } from 'react';
 import { Light, Dark } from './styles/themes';
+import { Device } from './styles/breackpoints';
 import styled, { ThemeProvider } from 'styled-components';
 import MyRoutes from './routes/routes';
 
@@ -10,26 +11,62 @@ function App() {
   const[themeuse, setTheme] = useState("dark");
   const theme = themeuse === "light" ? "light" : "dark";
   const themeStyle = theme === "light" ? Light : Dark;
+  const[sidebarOpen, setSidebarOpen] = useState(false);
 
   return (
     <>
-      <ThemeContext.Provider value={{theme, setTheme}}>
-        <ThemeProvider theme={ themeStyle }>
+      <ThemeContext.Provider value={{ theme, setTheme }}>
+        <ThemeProvider theme={themeStyle}>
           <AuthContextProvider>
-            <Container>
-              <MyRoutes/>
+            <Container className={sidebarOpen ? "active" : ""}>
+              <section className="ContentSidebar">
+                Sidebar
+              </section>
+              <section className="ContentMenuambur">
+                Hamburguer
+              </section>
+              <section className="ContentRoutes">
+                <MyRoutes />
+              </section>
             </Container>
           </AuthContextProvider>
         </ThemeProvider>
       </ThemeContext.Provider>
     </>
-  )
+  );
 }
 
 const Container = styled.div`
-  display: flex;
+  display: grid;
   grid-template-columns: 1fr;
-  background-color: ${({theme}) => theme.bgtotal};
+  background-color: ${({ theme }) => theme.bgtotal};
+  .ContentSidebar {
+    display: none;
+  }
+  .ContentMenuambur {
+    display: block;
+    position: absolute;
+    left: 20px;
+  }
+  @media ${Device.tablet} {
+    grid-template-columns: 65px 1fr;
+    &.active {
+      grid-template-columns: 220px 1fr;
+    }
+    .ContentSidebar {
+      display: initial;
+    }
+    .ContentMenuambur {
+      display: none;
+    }
+  }
+  .ContentRoutes {
+    grid-column: 1;
+    width: 100%;
+    @media ${Device.tablet} {
+      grid-column: 2;
+    }
+  }
 `;
 
 
--- src/components/template/HomeTemplate.jsx
+++ src/components/template/HomeTemplate.jsx
@@ -1,10 +1,24 @@
+import styled from "styled-components";
 
 const HomeTemplate = () => {
     return (
         <>
-            <h1>Home tamplate</h1>
+            <Container>
+                <h1>Home tamplate</h1>
+            </Container>
         </>
     )
 }
+
+const Container = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+    overflow: hidden;
+    background-color: ${(props) => props.theme.bgtotal};
+    color: ${({ theme }) => theme.text};
+    width: 100%;
+`;
 
 export default HomeTemplate
